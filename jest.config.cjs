532e0fe1bd9faf9e60a073ac39ef50e20fa06e28
SaiKumarReddy374 <saikumarreddypalakolanu6@gmail.com>
2025-05-07 21:21:13 +0530

    first commit

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,10 @@
 module.exports = {
-  testEnvironment: 'jsdom',
+  testEnvironment: "node", // Use Node.js environment for backend tests
+  testMatch: ["**/citests/**/*.test.js"], // Only look for test files in the citests folder
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    "^.+\\.jsx?$": "babel-jest", // Transform JavaScript files using Babel
   },
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy", // Mock CSS imports
   },
 };
